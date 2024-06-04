06c6a339e755cdf0294da1c83acf63f2cee834ee
d0n4to <meccadonato79@gmail.com>
2024-06-04 15:34:59 +0200

    .

--- script.js
+++ script.js
@@ -1,48 +1,61 @@
-/*                                                           -----------------------TEORIA-----------------------
+/* -----------------------TEORIA-----------------------
  Un'API (Application Programming Interface) è un insieme di definizioni, protocolli e strumenti che permette a diverse applicazioni software di comunicare tra loro. 
  Le API definiscono i metodi e i dati che le applicazioni possono utilizzare per interagire con altre applicazioni, servizi o sistemi.
- Nel codice sono utilizzate per cercare e visualizzare i film accedendo al TMBD 
- 
- async: utilizzata per dichiarare una funzione asincrona. Questa ritorna una Promise cioè un oggetto che rappresenta il completamneto o il fallimento di un operazione
- await: utilizzata nelle funzioni async per metterle in pausa l'esecuzione fino a qunado una funzione asincrona viene completata
-*/ 
+ Nel codice sono utilizzate per cercare e visualizzare i film accedendo al TMDB 
+
+ async: utilizzata per dichiarare una funzione asincrona. Questa ritorna una Promise cioè un oggetto che rappresenta il completamento o il fallimento di un'operazione
+ await: utilizzata nelle funzioni async per metterle in pausa l'esecuzione fino a quando una funzione asincrona viene completata
+*/
 
 // Event Listener che si attiva quando viene caricata tutta la pagina
 document.addEventListener('DOMContentLoaded', function () {
     const searchForm = document.getElementById('searchForm');
     const searchInput = document.getElementById('searchInput');
     const resultsDiv = document.getElementById('results');
+    const prevPageButton = document.getElementById('prevPage');
+    const nextPageButton = document.getElementById('nextPage');
+    const pageNumberSpan = document.getElementById('pageNumber');
+
+    let currentPage = 1; // Variabile per tenere traccia della pagina corrente
+    let moviesPerPage = 12; // Numero di film per pagina
+    let totalMovies = []; // Array per memorizzare i film totali
 
     // Caricamento dei film consigliati
     fetchAndDisplayRecommendedMovies();
 
     // Event Listener che si attiva quando viene premuto il tasto invio sulla searchbar
     searchForm.addEventListener('submit', async function (event) {
-        event.preventDefault(); // Evita il comportamento predefinito della searchbar ad esempio riaggiornare la pagina
+        event.preventDefault(); // Evita il comportamento predefinito della searchbar, ad esempio riaggiornare la pagina
         const searchQuery = searchInput.value.trim(); 
 
         // Controlla se searchQuery contiene un valore non vuoto
         if (searchQuery) {
-            const movies = await fetchMovies(searchQuery); // Ricerca film
-            displayResults(movies); // Mostra Film
+            const movies = await fetchMovies(searchQuery); // Ricerca dei film
+            totalMovies = movies; // Salva i film trovati nell'array totale
+            currentPage = 1; 
+            updatePagination();
+            displayResults(movies.slice(0, moviesPerPage)); // Mostra i risultati della prima pagina
         } else {
             resultsDiv.innerHTML = '';
-            fetchAndDisplayRecommendedMovies(); // Ricerca e Mostra i film consigliati
+            fetchAndDisplayRecommendedMovies(); // Carica e mostra i film consigliati
         }
     });
 
-    // Ricerca dei Film con l'API TMDb
+    // Event Listener per i pulsanti di paginazione
+    prevPageButton.addEventListener('click', () => changePage(-1));
+    nextPageButton.addEventListener('click', () => changePage(1));
+
+    // Funzione per cercare i film 
     async function fetchMovies(query) {
         const apiKey = '801056529bee61b32d6106f6327cb8f4';
         const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(query)}`;
         const movies = [];
 
         try {
-            const response = await fetch(apiUrl);
-            const data = await response.json();
-
+            const response = await fetch(apiUrl); // Richiesta HTTP get
+            const data = await response.json(); // Legge il corpo della risposta HTTP
             if (data.results && data.results.length > 0) {
-                movies.push(...data.results);
+                movies.push(...data.results); // Aggiunta dei risultati all'array di film
             } else {
                 console.log('No results found');
             }
@@ -53,42 +66,24 @@ document.addEventListener('DOMContentLoaded', function () {
         return movies;
     }
 
-
-    // Funzione per porzionare ogni riga in 4 colonne
-    function chunkArray(arr, size) {
-        // chucks = accumulatore delle porzioni   item = l'elemento dell'array  i = indice dell'eleemnto
-        return arr.reduce((chunks, item, i) => {
-            // Contollo se è il contatore è multiplo di 4 
-            if (i % size === 0) {
-                chunks.push([item]); // Aggiunta dell'item ad un nuovo array chunks
-            } else {
-                chunks[chunks.length - 1].push(item); // Aggiunta dell'item ad un chunks in costruzione
-            }
-            return chunks;
-        }, []);
-    }
-
-    // Funzione per visualizzare i film
+    // Funzione per visualizzare i risultati dei film
     function displayResults(movies) {
-        resultsDiv.innerHTML = '';
-
-        // Controlla se ci sono film
+        resultsDiv.innerHTML = ''; 
         if (movies.length > 0) {
-            const rows = chunkArray(movies, 4); // Porziona le righe in 4 colonne
+            const rows = chunkArray(movies, 4); // Dividi i film in righe di 4
             rows.forEach(row => {
-
                 const rowContainer = document.createElement('div');
                 rowContainer.style.display = 'flex';
                 rowContainer.style.flexWrap = 'wrap';
                 rowContainer.style.justifyContent = 'center';
                 rowContainer.style.margin = '20px 0px';
-    
+
                 row.forEach(movie => {
                     // Controlla se esiste l'immagine del film
                     if (movie.poster_path) {
                         const listItem = document.createElement('div');
                         listItem.classList.add('movie-item');
-    
+
                         listItem.innerHTML = `
                             <div class="container">
                                 <div class="row">
@@ -111,71 +106,138 @@ document.addEventListener('DOMContentLoaded', function () {
                                 </div>
                             </div>
                         `;
-    
+
                         // Evento di click per reindirizzare alla pagina di descrizione
                         listItem.addEventListener('click', function () {
                             const tmdbID = this.querySelector('img').getAttribute('data-imdbid');
                             fetchMovieDetails(tmdbID);
                         });
-
-                        rowContainer.appendChild(listItem); // Aggiunta dell'film alla riga
+                        rowContainer.appendChild(listItem); // Aggiunta del film alla riga
                     }
                 });
                 resultsDiv.appendChild(rowContainer); // Aggiunta della riga al div
             });
         } else {
-            showNoResultsMessage(); // Messaggio di errore
+            showNoResultsMessage(); // Mostra il messaggio di nessun risultato
         }
     }
 
-    // Funzione per mostrare il messaggio d'errore
+    // Funzione per porzionare ogni riga in 4 colonne
+    function chunkArray(arr, size) {
+        // chucks = accumulatore delle porzioni   item = l'elemento dell'array  i = indice dell'eleemnto
+        return arr.reduce((chunks, item, i) => {
+            // Contollo se è il contatore è multiplo di 4 
+            if (i % size === 0) {
+                chunks.push([item]); // Inizia una nuova porzione
+            } else {
+                chunks[chunks.length - 1].push(item); // Aggiunge l'elemento alla porzione corrente
+            }
+            return chunks;
+        }, []);
+    }
+
+    // Funzione per mostrare il messaggio di nessun risultato
     function showNoResultsMessage() {
         resultsDiv.innerHTML = '';
-    
         var icon = document.createElement('img');
         icon.src = 'images/notFound.png';
         icon.alt = 'No results found';
-    
         var message = document.createElement('p');
-        message.textContent = 'Not results found';
+        message.textContent = 'No results found';
         message.style.color = 'white';
-
-        // Aggiunta del'icona e del messaggio al div
         resultsDiv.appendChild(icon);
         resultsDiv.appendChild(message);
     }
 
-    // Ricerca e Visualizzazione dei film consigliati
+    // Funzione per cercare e visualizzare i film consigliati
     async function fetchAndDisplayRecommendedMovies() {
         try {
             const popularMovies = await fetchPopularMovies();
-            displayResults(popularMovies);
+            totalMovies = popularMovies; // Salva i film popolari nell'array totale
+            currentPage = 1; 
+            updatePagination(); 
+            displayResults(popularMovies.slice(0, moviesPerPage)); // Mostra i risultati della prima pagina
         } catch (error) {
             console.error('Error fetching and displaying popular movies:', error);
         }
     }
 
-    // Ricerca dei film consigliati
+    // Funzione per cercare i film popolari 
     async function fetchPopularMovies() {
-        const apiKey = '801056529bee61b32d6106f6327cb8f4'; // API TMDb
+        const apiKey = '801056529bee61b32d6106f6327cb8f4';
         const apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&sort_by=popularity.desc`;
-    
         try {
             const response = await fetch(apiUrl); // Richiesta HTTP get
-            const data = await response.json(); // Legge il corpo della risposta HTTP
-            return data.results;
+            const data = await response.json();  // Legge il corpo della risposta HTTP
+            return data.results; 
         } catch (error) {
             console.error('Error fetching popular movies:', error);
             return [];
         }
     }
+
+    // Funzione per aggiornare la paginazione
+    function updatePagination() {
+        const totalPages = Math.ceil(totalMovies.length / moviesPerPage);
+        pageNumberSpan.textContent = currentPage;
+        prevPageButton.disabled = currentPage === 1;
+        nextPageButton.disabled = currentPage === totalPages;
+    }
+
+    // Funzione per cambiare pagina
+    function changePage(direction) {
+        const totalPages = Math.ceil(totalMovies.length / moviesPerPage);
+        currentPage += direction; // Aggiorna la pagina corrente
+        currentPage = Math.max(1, Math.min(currentPage, totalPages)); // Assicura che la pagina corrente sia valida
+        const start = (currentPage - 1) * moviesPerPage;
+        const end = start + moviesPerPage;
+        displayResults(totalMovies.slice(start, end)); // Mostra i risultati della nuova pagina
+        updatePagination(); // Aggiorna la paginazione
+    }
+
+    // Event Listener per il bottone di film casuali
+    const randomMoviesButton = document.getElementById('randomMoviesButton');
+    randomMoviesButton.addEventListener('click', async () => {
+        try {
+            const randomMovies = await fetchRandomMovies(); // Funzione per recuperare film casuali
+            totalMovies = randomMovies; // Salva i film casuali nell'array totale
+            currentPage = 1;
+            updatePagination();
+            displayResults(randomMovies.slice(0, moviesPerPage)); // Mostra i risultati della prima pagina
+        } catch (error) {
+            console.error('Error fetching and displaying random movies:', error);
+        }
+    });
+
+    // Funzione per recuperare film casuali
+    async function fetchRandomMovies() {
+        const apiKey = '801056529bee61b32d6106f6327cb8f4';
+        const totalPages = 500; // Limite massimo consentito di pagine
+        const randomPage = Math.floor(Math.random() * totalPages) + 1; // Genera un numero casuale tra 1 e totalPages
+        const apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&sort_by=popularity.desc&page=${randomPage}`;
+
+        try {
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+
+            // Controlla se i dati sono validi
+            if (response.ok && data.results && data.results.length > 0) {
+                const randomMovies = data.results;
+                return randomMovies;
+            } else {
+                throw new Error('Invalid data received from API');
+            }
+        } catch (error) {
+            console.error('Error fetching random movies:', error);
+            return [];
+        }
+    }
+
 });
 
-// Reindirazzamento alla pagina delle descrizioni dei singoli film
+
+// Funzione per reindirizzare alla pagina di descrizione del film
 function fetchMovieDetails(tmdbID) {
-    // URL della pagina description.html con l'ID del film come parametro
     const url = `description.html?tmdbID=${tmdbID}`;
-    
-    // Reindirizza alla pagina description.html
     window.location.href = url;
 }
